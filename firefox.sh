165bc05cd532143c48496537b1a501af0b4b9fe6
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-11-26 19:13:16 +0000

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -4,11 +4,9 @@ sudo apt-get install -y puppet git
 
 sudo apt-get purge firefox
 
-cd /etc/puppet/modules
-
 git clone https://github.com/mhyvarinen/firefox.git
 
-cd /home/xubuntu
+sudo mv -r firefox /etc/puppet/modules/
 
 git clone https://github.com/mhyvarinen/firefox-site.pp.git
 
