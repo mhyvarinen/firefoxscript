d27aa00f7eeda8f4e83e809862f092dadd4cf4f2
mhyvarinen <31437091+mhyvarinen@users.noreply.github.com>
2017-11-26 19:03:12 +0000

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -8,7 +8,13 @@ cd /etc/puppet/modules
 
 git clone https://github.com/mhyvarinen/firefox.git
 
-sudo puppet apply /etc/puppet/manifests/site.pp
+cd /home/xubuntu
+
+git clone https://github.com/mhyvarinen/firefox-site.pp.git
+
+cd /home/xubuntu/firefox-site.pp/
+
+sudo mv site.pp /etc/puppet/manifests/
 
 echo " "
 echo "Asennus valmis"
